63381e51b4ae7a5703b18f96b3d776a1f8a86323
Hamzaabro5 <hamzaabro683@gmail.com>
2024-03-20 17:03:12 +0500

    Second Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const input = document.querySelector(`#input-bar`)
+const list = document.querySelector(`.ul`)
+
+function addTask() {
+    if (input.value == ``) {
+        alert(`Please Enter Your Task `)
+    } else {
+        
+    }
+}
