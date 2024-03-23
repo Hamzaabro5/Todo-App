a70e30324f5b260b7687cb136200fe2536a5b38a
Hamzaabro5 <hamzaabro683@gmail.com>
2024-03-23 17:34:15 +0500

    Final Output

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const input = document.querySelector(`#input-bar`)
-const list = document.querySelector(`.li`)
+const ol = document.querySelector(`.ol`)
+const arr = []
 
+function renderTodo() {
+    
+    ol.innerHTML = ``
+    
+   for (let i = 0; i < arr.length; i++) {
+    ol.innerHTML += `<ol>${arr[i]}
+    <button type="button" class="btn btn-danger text-align-center" onclick="deleteTodo(${i})">Delete</button>
+    <button type="button" class="btn btn-success" onclick = "editTodo(${i})">Edit</button>
+    </ol>`
+    
+   }
+}
 
 function addTask() {
-    if (input.value == ``) {
-        alert(`Please Enter Your Task `)
-    } else {
-       list.innerHTML = input.value
-       input.value = ``
-    }
+    
+    arr.push(input.value)
+    console.log(arr);
+    renderTodo()
+    input.value = ``
+}
+
+function deleteTodo(index) {
+    arr.splice(index, 1)
+    renderTodo()
+}
+
+function editTodo(index) {
+    const updatevalue = prompt(`Enter Your Updated Value?`)
+    arr.splice(index, 1, updatevalue)
+    renderTodo()
 }
